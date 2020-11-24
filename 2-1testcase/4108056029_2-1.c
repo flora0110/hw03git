40352f31ea3a0548d5b4e3594890228265de1921
flora <flora90.1.10@gmail.com>
2020-11-24 08:55:02 +0800

    完成

---
+++ 2-1testcase/4108056029_2-1.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+char mose[26][5]={{'.','-'},{'-','.','.','.'},{'-','.','-','.'},{'-','.','.'},{'.'},\
+    {'.','.','-','.'},{'-','-','.'},{'.','.','.','.'},{'.','.'},{'.','-','-','-'},\
+    {'-','.','-'},{'.','-','.','.'},{'-','-'},{'-','.'},{'-','-','-'},\
+    {'.','-','-','.'},{'-','-','.','-'},{'.','-','.'},{'.','.','.'},{'-'},\
+    {'.','.','-'},{'.','.','.','-'},{'.','-','-'},{'-','.','.','-'},{'-','.','-','-'},{'-','-','.','.'}};
+char* trans(char* word){
+    int i,j;
+    int n=0;
+    /*
+    printf("%c\n",mose[1][5]);
+    for(i=0;i<26;i++){
+        printf("%d %d\n",i,strlen(mose[i]));
+    }*/
+    //printf("%d\n",strlen(mose[0]));
+    char *result=(char*)malloc(400*sizeof(char));
+    for(i=0;i<strlen(word);i++){
+        //printf("%c %d\n",word[i],word[i] );
+        //printf("%d\n",strlen(mose[word[i]-65]) );
+        for(j=0;j<strlen(mose[word[i]-65]);j++){
+            result[n++]=mose[word[i]-65][j];
+            //printf("%c",mose[word[i]-65][j]);
+        }
+        //printf("\n");
+    }
+    return result;
+}
+int indiction(char* code,char** diction,int n){
+    int i,j;
+    int check=0;
+    for(i=0;i<n;i++){
+        //printf("codelen: %d diclen: %d\n",strlen(code),strlen(diction[i]));
+        if(strlen(code)!=strlen(diction[i])){
+            continue;
+        }
+        for(j=0;j<strlen(code);j++){
+            check=1;
+            if(code[j]!=diction[i][j]){
+                check=0;
+            }
+        }
+        if(check==1){
+            return 1;
+        }
+    }
+    return check;
+}
+int main(){
+    FILE *rptr;
+    FILE *wptr;
+    rptr=fopen("test1.txt","r");
+    wptr=fopen("output2-1.txt","w");
+    if(rptr==NULL|| wptr==NULL){
+        printf("open error\n");
+        return 0;
+    }
+    int n,m,i,j;
+    char word[100];
+    char code[400];
+    fscanf(rptr,"%d\n",&n);
+    char *ans=(char*)malloc(400*sizeof(char));
+    char **diction=(char**)malloc(n*sizeof(char*));
+    for(i=0;i<n;i++){
+        fscanf(rptr,"%[A-Z]\n",&word);
+        //test---------------
+        printf("%d \n",strlen(word));
+        for(j=0;j<strlen(word);j++){
+            printf("%c",word[j]);
+        }
+        printf("\n");
+        //------------------
+        ans=trans(word);
+        diction[i]=ans;
+        for(j=0;j<strlen(ans);j++){
+            printf("%c",ans[j]);
+        }
+        printf("\n");
+    }
+    fscanf(rptr,"%d\n",&m);
+
+    for(i=0;i<m;i++){
+        fscanf(rptr,"%[.-]\n",&code);
+        //test---------------
+        printf("%d \n",strlen(code));
+        for(j=0;j<strlen(code);j++){
+            printf("%c",code[j]);
+        }
+        printf("\n");
+        //------------------
+        if(indiction(code,diction,n)){
+            printf("It is found in the dictionary.\n");
+        }
+    }
+}
