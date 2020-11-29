8902ddf3e8e79e53fa87db447a0e276cded01ffe
flora <flora90.1.10@gmail.com>
2020-11-30 05:44:17 +0800

    樹完成

--- 2-3testcase/4108056029_2-3.c
+++ 2-3testcase/4108056029_2-3.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+void find(char*,int);
+long long check(char*,int);
 char mose[26][5]={{'.','-'},{'-','.','.','.'},{'-','.','-','.'},{'-','.','.'},{'.'},\
     {'.','.','-','.'},{'-','-','.'},{'.','.','.','.'},{'.','.'},{'.','-','-','-'},\
     {'-','.','-'},{'.','-','.','.'},{'-','-'},{'-','.'},{'-','-','-'},\
@@ -35,28 +37,107 @@ char* pop(){
     return item;
 }
 
+typedef struct node* nodepointer;
+typedef struct node{
+    int data;
+    nodepointer dot;
+    nodepointer dash;
+}node;
+nodepointer root;
 char* trans(char* word){
     int i,j;
     int n=0;
-    char *result=(char*)malloc(400*sizeof(char));
+    char *result=(char*)malloc(401*sizeof(char));
     for(i=0;i<strlen(word);i++){
         for(j=0;j<strlen(mose[word[i]-65]);j++){
             result[n++]=mose[word[i]-65][j];
+            //printf("%c\n",result[n-1]);
         }
         result[n]='\0';
     }
-    //printf("%d %d\n",strlen(result),n );
-    /*for(i=0;i<n;i++){
-        printf("%c",result[i] );
+    //printf("%d\n",strlen(result));
+    nodepointer temp=root;
+    for(i=0;i<strlen(result);i++){
+        //printf("in for %c\n",result[i]);
+        if(result[i]=='.'){
+            //printf("inn\n" );
+            if(temp->dot==NULL){
+                nodepointer new=(node*)malloc(sizeof(node));
+                new->dot=NULL;
+                new->dash=NULL;
+                new->data=-1;
+                temp->dot=new;
+                //printf("dot\n");
+            }
+            temp=temp->dot;
+        }
+        else if(result[i]=='-'){
+            if(temp->dash==NULL){
+                nodepointer new=(node*)malloc(sizeof(node));
+                new->dot=NULL;
+                new->dash=NULL;
+                new->data=-1;
+                temp->dash=new;
+                //printf("dash\n");
+            }
+            temp=temp->dash;
+        }
     }
-    printf("\n");*/
+    temp->data=n;
+    //printf("result\n");
+    //system("pause");
     return result;
 }
 long long sum=0;
 int *step;
 long long *arrive;
-long long check(char* code,char** diction,int now,int dicn){//dicn是字典量
+void find(char* code,int now){
+    //printf("ffffffffffffffffffffffffffffffffff\n" );
+    nodepointer temp=root;
+    printf("in find now %d\n",now );
+    int orinow=now;
+    int i;
+    for(i=now;i<strlen(code);i++){
+        printf("%c",code[i] );
+    }
+    printf("\n");
+    do{
+        //printf("in do\n");
+        printf("%c\n",code[now]);
+
+        if(code[now]=='.'){
+            temp=temp->dot;
+            now++;
+            //printf("dot\n");
+        }
+        else if(code[now]=='-'){
+            temp=temp->dash;
+            now++;
+            //printf("dash\n");
+        }
+        if(!temp){
+            break;
+        }
+        //printf("kkk\n");
+        if(temp->data!=-1 && temp){
+            //printf("222\n");
+            printf("result %d\n",now);
+            printf("before ++ing arrive[%d] = %lld\n",orinow,arrive[orinow]);
+            arrive[orinow]+=check(code,now);
+            printf("++ing arrive[%d] = %lld\n",orinow,arrive[orinow]);
+        }
+        //printf("333\n");
+        printf("%d\n",now );
+    }while(temp && now<strlen(code));
+    printf("--------------over\n");
+    //printf("ooooooooooooooooooooooooooooooofffffffffffffff\n" );
+    return;
+}
+
+long long check(char* code,int now){//dicn是字典量
     //printf("now %d\n",now );
+    //printf("cccccccccccccccccccccccccccccccccc\n");
+    printf("in ckeck now %d\n",now);
     int i,j,t;
     if(now==strlen(code)){
         sum++;
@@ -76,7 +157,17 @@ long long check(char* code,char** diction,int now,int dicn){//dicn是字典量
         //test-----------*/
         return 1;
     }
-    for(i=0;i<dicn;i++){
+    if(!step[now]){
+        sum+=arrive[now];
+        printf("--------------     %lld\n",sum);
+        printf("add arrive[%d] (%lld) = %lld\n",now,arrive[now],sum );
+        //printf("------------------------------------------------------\n");
+        return arrive[now];
+    }
+    else{
+        find(code,now);
+    }
+    /*for(i=0;i<dicn;i++){
         t=1;
         if(strlen(diction[i])>strlen(code)){
             t=0;
@@ -93,30 +184,23 @@ long long check(char* code,char** diction,int now,int dicn){//dicn是字典量
             //push(diction[i]);
             //----
             //printf("add diction: %d\n",i );
-            if(step[now]==1){
                 //printf("before ++ing arrive[%d] = %lld\n",now,arrive[now]);
                 arrive[now]+=check(code,diction,now+strlen(diction[i]),dicn);
                 //printf("++ing arrive[%d] = %lld\n",now,arrive[now]);
-            }
-            else{
-                sum+=arrive[now];
-                printf("--------------     %lld\n",sum );
-                //printf("add arrive[%d] (%lld) = %lld\n",now,arrive[now],sum );
-                return arrive[now];
-            }
             //test---
             //pop();
             //---
         }
-    }
+    }*/
     step[now]=0;
-    //printf("arrive[%d] = %lld\n",now,arrive[now]);
+    printf("arrive[%d] = %lld\n",now,arrive[now]);
+    //printf("-----------------------------now-------------------------\n");
     return arrive[now];
 }
 int main(){
     FILE *rptr;
     FILE *wptr;
-    rptr=fopen("test4.txt","r");
+    rptr=fopen("test2.txt","r");
     wptr=fopen("output2-3.txt","w");
     if(rptr==NULL|| wptr==NULL){
         printf("open error\n");
@@ -127,11 +211,15 @@ int main(){
     printf("%d\n",sizeof(testlong));
     int n;
     int i,j;
+    root=(nodepointer)malloc(sizeof(node));
+    root->data=-1;
+    root->dot=NULL;
+    root->dash=NULL;
     char *code=(char*)malloc(100001*sizeof(char));
     fscanf(rptr,"%[-.]\n",code);
     //test---------
     printf("strlen %d\n",strlen(code));
-    /*for(i=0;i<strlen(code);i++){
+    for(i=0;i<strlen(code);i++){
         printf("%c",code[i] );
     }
     printf("\n");
@@ -160,7 +248,7 @@ int main(){
         printf("-----------------\n");*/
         trcode=trans(word);
         diction[i]=trcode;
-        /*//test-----
+        //test-----
         for(j=0;j<strlen(diction[i]);j++){
             printf("%c",diction[i][j]);
         }
@@ -168,6 +256,7 @@ int main(){
         //test-----*/
     }
     printf("tran over\n");
-    check(code,diction,0,n);
-    printf("output: %lld\n",sum );
+    check(code,0);
+    printf("outt\n");
+    printf("output: %lld\n",arrive[0]);
 }
